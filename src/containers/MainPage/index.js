941f666d6d070b9c2fa3dd6bbcc5a45b78d2fedd
Aaron Weissman <weissman.aaron@gmail.com>
2018-12-24 12:09:02 -0700

    bring in search bar

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { Route, withRouter, Switch } from 'react-router-dom';
 
+import './MainPage.css';
+
+import SearchBar from '../../components/SearchBar';
 import DataSlider from '../DataSlider';
 
 class MainPage extends Component {
   render() {
     return (
       <div className="main-page">
-        <h1>Main Page</h1>
-        <h2>{this.props.username}</h2>
+        <SearchBar />
         <DataSlider />
       </div>
     );
