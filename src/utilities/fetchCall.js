ad975acc29d8a5e48ad80fa28be0e0c1e46a0d18
Aaron Weissman <weissman.aaron@gmail.com>
2018-12-26 08:44:21 -0700

    add fetchcall

---
+++ src/utilities/fetchCall.js
@@ -0,0 +1,9 @@
+const fetchCall = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return Error(response.statusText);
+  }
+  return await response.json();
+}
+
+export default fetchCall;
