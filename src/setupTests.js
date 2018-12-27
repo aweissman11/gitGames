22156b3df9e750a4159fdba0f95b9eb75dc69cb1
Aaron Weissman <weissman.aaron@gmail.com>
2018-12-27 08:57:50 -0700

    overwrite matchMedia for slider module in jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,15 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+
+global.matchMedia = global.matchMedia || function() {
+  return {
+      matches : false,
+      addListener : function() {},
+      removeListener: function() {}
+  }
+}
+
 configure({ adapter: new Adapter() });
 
 class LocalStorage{
