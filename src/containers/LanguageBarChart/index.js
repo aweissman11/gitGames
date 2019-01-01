23e2d4f1955a41a8a5942b4137ff35eb97cbb288
Aaron Weissman <weissman.aaron@gmail.com>
2019-01-01 15:03:33 -0700

    bar chart styling and loading handling

--- src/containers/LanguageBarChart/index.js
+++ src/containers/LanguageBarChart/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import ReactLoading from 'react-loading';
 import { ResponsiveBar } from '@nivo/bar'
+
+import './LanguageBarChart.css';
+import SliderLoading from '../../components/SliderLoading';
 
 export class LanguageBarChart extends Component {
   constructor(props) {
@@ -36,8 +39,15 @@ export class LanguageBarChart extends Component {
       return acc;
     }, {})
 
-    console.log('languagedata:', data);
     return data;
+    // return {
+    //   "HTML": "0.47",
+    //   "hover": "percent used",
+    //   "CSS": "0.61",
+    //   "JavaScript": "98.62",
+    //   "Roff": "0.04",
+    //   "Ruby": "0.26",
+    // }
   }
 
 
@@ -45,65 +55,67 @@ export class LanguageBarChart extends Component {
     let data = this.cleanedUpData()
 
     return (
-      <div style={{width: '960px', height: '600px'}}>
-        <ResponsiveBar
-            data={[data]}
-            keys={Object.keys(data)}
-            indexBy="hover"
-            margin={{
-                "top": 50,
-                "right": 130,
-                "bottom": 50,
-                "left": 60
-            }}
-            padding={0.3}
-            layout="horizontal"
-            colors="yellow_orange_red"
-            colorBy="id"
-            reverse={true}
-
-            borderColor="inherit:darker(1.6)"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={null}
-            axisLeft={null}
-            enableGridY={false}
-            labelSkipWidth={12}
-            labelSkipHeight={12}
-            labelTextColor="inherit:darker(1.6)"
-            animate={true}
-            motionStiffness={90}
-            motionDamping={15}
-            legends={[
-                {
-                    "dataFrom": "keys",
-                    "anchor": "bottom-right",
-                    "direction": "column",
-                    "justify": false,
-                    "translateX": 120,
-                    "translateY": -200,
-                    "itemsSpacing": 2,
-                    "itemWidth": 100,
-                    "itemHeight": 20,
-                    "itemDirection": "left-to-right",
-                    "itemOpacity": 0.85,
-                    "symbolSize": 20,
-                    "effects": [
-                        {
-                            "on": "hover",
-                            "style": {
-                                "itemOpacity": 1
+      this.props.loadingLanguages.loadingLanguages ? <SliderLoading msg='Looking up language usage' /> :
+        <div className='bar-holder'>
+          <div style={{margin: 'auto', width: '90%', height: '300px', fontSize: '1em'}}>
+            <ResponsiveBar
+                className='responsive-bar'
+                data={[data]}
+                keys={Object.keys(data)}
+                indexBy="hover"
+                margin={{
+                    "top": 50,
+                    "right": 130,
+                    "bottom": 50,
+                    "left": 60
+                }}
+                padding={0.3}
+                layout="horizontal"
+                colors="blue_purple"
+                colorBy="id"
+                reverse={false}
+                borderColor="inherit:darker(1.6)"
+                axisTop={null}
+                axisRight={null}
+                axisBottom={null}
+                axisLeft={null}
+                enableGridY={false}
+                labelSkipWidth={12}
+                labelTextColor="inherit:darker(1.6)"
+                animate={true}
+                motionStiffness={90}
+                motionDamping={15}
+                legends={[
+                    {
+                        "dataFrom": "keys",
+                        "anchor": "bottom-right",
+                        "direction": "column",
+                        "justify": false,
+                        "translateX": 120,
+                        "translateY": -55,
+                        "itemsSpacing": 2,
+                        "itemWidth": 100,
+                        "itemHeight": 20,
+                        "itemDirection": "left-to-right",
+                        "itemOpacity": 0.85,
+                        "symbolSize": 20,
+                        "effects": [
+                            {
+                                "on": "hover",
+                                "style": {
+                                    "itemOpacity": 1
+                                }
                             }
-                        }
-                    ]
-                }
-            ]}
-        />
-      </div>
+                        ]
+                    }
+                ]}
+            />
+          </div>
+        </div>
     );
   }
 }
 
-export const mapStateToProps = ({ languageData }) => ({ languageData })
+export const mapStateToProps = ({ languageData, loadingLanguages }) => ({ languageData, loadingLanguages })
 
 export default connect(mapStateToProps, null)(LanguageBarChart);
