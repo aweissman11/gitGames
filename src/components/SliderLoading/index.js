d3a25d57e5d2b16f316ace31be15b70d6ae6c696
Aaron Weissman <weissman.aaron@gmail.com>
2019-01-01 15:02:52 -0700

    slider loading component

---
+++ src/components/SliderLoading/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+import './SliderLoading.css';
+
+export const SliderLoading = (props) => (
+  <div className='slider-loading'>
+    <h4 className='slider-loading-text'>
+    <span>
+      <ReactLoading className='slider-loading-spinner' type={'spokes'} color={'blueviolet'} height={40} width={40} />
+    </span>
+    {props.msg}
+    </h4>
+  </div>
+);
+
+export default SliderLoading;
