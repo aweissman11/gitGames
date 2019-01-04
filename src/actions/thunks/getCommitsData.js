8dc28f4276d64728ce9c0f9f90282327f435e834
Aaron Weissman <weissman.aaron@gmail.com>
2019-01-03 23:14:22 -0700

    get  commits thunk

---
+++ src/actions/thunks/getCommitsData.js
@@ -0,0 +1,20 @@
+import { fetchCall } from '../../utilities/fetchCall';
+
+import { loadingCommits, hasErrored, setCommitsData } from '../'
+
+export const getCommitsData = (user) => {
+  return async (dispatch) => {
+    let commitsData;
+    dispatch(loadingCommits('Getting Commits Data', true))
+
+    try {
+      const cloudUrl = `https://cors-anywhere.herokuapp.com/gitgames.herokuapp.com/api/v1/commit_timelines?username=${user.username}`
+      commitsData = await fetchCall(cloudUrl)
+    } catch(error) {
+      dispatch(hasErrored('Commits data fetch failed', true))
+      return;
+    }
+    dispatch(setCommitsData(commitsData))
+    dispatch(loadingCommits('Commits data retrieved', false))
+  }
+}
