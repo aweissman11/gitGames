7ff1f1be2e30fc3a673d0dfd0014130d2ee2cab3
Aaron Weissman <weissman.aaron@gmail.com>
2019-01-21 15:13:33 -0700

    ready to deploy

--- src/actions/thunks/getCommitsData.js
+++ src/actions/thunks/getCommitsData.js
@@ -1,28 +1,3 @@
-// import { fetchCall } from '../../utilities/fetchCall';
-
-// import { loadingCommits, hasErrored, setCommitsData } from '../'
-
-// import { mockCommitsData } from './__mocks__/mockCommitsData';
-
-// export const getCommitsData = (user) => {
-//   return async (dispatch) => {
-//     let commitsData;
-//     dispatch(loadingCommits('Getting Commits Data', true))
-
-//     try {
-//       const cloudUrl = `https://cors-anywhere.herokuapp.com/gitgames.herokuapp.com/api/v1/commit_timelines?username=${user.username}`
-//       commitsData = await fetchCall(cloudUrl)
-//     } catch(error) {
-//       dispatch(hasErrored('Commits data fetch failed', true))
-//       return;
-//     }
-
-//     dispatch(setCommitsData(commitsData))
-//     dispatch(loadingCommits('Commits data retrieved', false))
-//   }
-// }
-
-
 import { fetchCall } from '../../utilities/fetchCall';
 
 import { loadingCommits, hasErrored, setCommitsData } from '../'
@@ -33,7 +8,32 @@ export const getCommitsData = (user) => {
   return async (dispatch) => {
     let commitsData;
     dispatch(loadingCommits('Getting Commits Data', true))
-    dispatch(setCommitsData(mockCommitsData))
+
+    try {
+      const cloudUrl = `https://cors-anywhere.herokuapp.com/gitgames.herokuapp.com/api/v1/commit_timelines?username=${user.username}`
+      commitsData = await fetchCall(cloudUrl)
+    } catch(error) {
+      dispatch(hasErrored('Commits data fetch failed', true))
+      return;
+    }
+
+    dispatch(setCommitsData(commitsData))
     dispatch(loadingCommits('Commits data retrieved', false))
   }
 }
+
+
+// import { fetchCall } from '../../utilities/fetchCall';
+
+// import { loadingCommits, hasErrored, setCommitsData } from '../'
+
+// import { mockCommitsData } from './__mocks__/mockCommitsData';
+
+// export const getCommitsData = (user) => {
+//   return async (dispatch) => {
+//     let commitsData;
+//     dispatch(loadingCommits('Getting Commits Data', true))
+//     dispatch(setCommitsData(mockCommitsData))
+//     dispatch(loadingCommits('Commits data retrieved', false))
+//   }
+// }
--- src/actions/thunks/getUserData.js
+++ src/actions/thunks/getUserData.js
@@ -1,42 +1,3 @@
-// import { fetchCall } from '../../utilities/fetchCall';
-
-// import { loadingUser, hasErrored, setUserData } from '../'
-// import { getCloudData } from './getCloudData';
-// import { getLanguageData } from './getLanguagesData';
-// import { getCommunityData } from './getCommunityData';
-// import { getCommitsData } from './getCommitsData';
-
-// import { mockUserData } from './__mocks__/mockUserData';
-
-// export const getUserData = (user) => {
-//   return async (dispatch) => {
-//     let userData;
-//     dispatch(loadingUser('Getting User Data', true))
-    
-//     try {
-//       const userUrl = `https://cors-anywhere.herokuapp.com/gitgames.herokuapp.com/api/v1/users?username=${user.username}`
-//       userData = await fetchCall(userUrl)
-//       if (userData.error) {
-//         dispatch(hasErrored(userData.error, true))
-//         dispatch(loadingUser('User data has errored', false))
-//         return;
-//       }
-//     } catch(error) {
-//       dispatch(hasErrored('User data fetch failed', true))
-//       return;
-//     }
-    
-//     dispatch(getCloudData(user))
-//     dispatch(getLanguageData(user))
-//     dispatch(getCommitsData(user))
-//     dispatch(getCommunityData(user))
-
-//     dispatch(setUserData(userData));
-//     dispatch(loadingUser('User data retrieved', false))
-//   }
-// }
-
-
 import { fetchCall } from '../../utilities/fetchCall';
 
 import { loadingUser, hasErrored, setUserData } from '../'
@@ -52,12 +13,51 @@ export const getUserData = (user) => {
     let userData;
     dispatch(loadingUser('Getting User Data', true))
     
+    try {
+      const userUrl = `https://cors-anywhere.herokuapp.com/gitgames.herokuapp.com/api/v1/users?username=${user.username}`
+      userData = await fetchCall(userUrl)
+      if (userData.error) {
+        dispatch(hasErrored(userData.error, true))
+        dispatch(loadingUser('User data has errored', false))
+        return;
+      }
+    } catch(error) {
+      dispatch(hasErrored('User data fetch failed', true))
+      return;
+    }
+    
     dispatch(getCloudData(user))
     dispatch(getLanguageData(user))
     dispatch(getCommitsData(user))
     dispatch(getCommunityData(user))
 
-    dispatch(setUserData(mockUserData));
+    dispatch(setUserData(userData));
     dispatch(loadingUser('User data retrieved', false))
   }
 }
+
+
+// import { fetchCall } from '../../utilities/fetchCall';
+
+// import { loadingUser, hasErrored, setUserData } from '../'
+// import { getCloudData } from './getCloudData';
+// import { getLanguageData } from './getLanguagesData';
+// import { getCommunityData } from './getCommunityData';
+// import { getCommitsData } from './getCommitsData';
+
+// import { mockUserData } from './__mocks__/mockUserData';
+
+// export const getUserData = (user) => {
+//   return async (dispatch) => {
+//     let userData;
+//     dispatch(loadingUser('Getting User Data', true))
+    
+//     dispatch(getCloudData(user))
+//     dispatch(getLanguageData(user))
+//     dispatch(getCommitsData(user))
+//     dispatch(getCommunityData(user))
+
+//     dispatch(setUserData(mockUserData));
+//     dispatch(loadingUser('User data retrieved', false))
+//   }
+// }
